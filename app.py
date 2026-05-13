bfe4a8da91ce311c8559bd8301cd62e96fa5b6d6
divyamerugu76-netizen <divyamerugu76@gmail.com>
2026-05-13 08:18:10 +0530

    Fix Dockerfile

--- app.py
+++ app.py
@@ -260,10 +260,12 @@ def predict(body: PredictIn):
 def root():
     return {"status": "NexaBank API running ✓"}
 
-import os
+if __name__ == "__main__":
+    import uvicorn
+    import os
 
-uvicorn.run(
-    app,
-    host="0.0.0.0",
-    port=int(os.environ.get("PORT", 7860))
-)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 7860))
+    )
